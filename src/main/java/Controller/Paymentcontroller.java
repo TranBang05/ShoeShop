26271562b2328e946063c62e533f9cba1e109967
bangtxhe18 <tranxuanbang.yt@gmail.com>
2023-07-06 19:22:29 +0700

    paymentr

---
+++ src/main/java/Controller/Paymentcontroller.java
@@ -0,0 +1,18 @@
+package Controller;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Paymentcontroller", value = "/Paymentcontroller")
+public class Paymentcontroller extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
